81db82d00327400a086dfe969bfa37f87f857c6c
ARVIND NAGAONKAR <arvind.nagaonkar.pro@gmail.com>
2021-07-08 13:46:03 +0530

    domain-name to Kurebuddy

--- src/Pages/Home Page/footer/Footer.js
+++ src/Pages/Home Page/footer/Footer.js
@@ -111,7 +111,7 @@ function Footer() {
           <div className="copyrightInput">
             <span>
               Copyright <i className="fas fa-copyright"></i> 2021 -All Rights
-              Reserved - Domain Name
+              Reserved - Kurebuddy
             </span>
           </div>
           <div className="copyrightInput">
@@ -132,7 +132,7 @@ function Footer() {
         <div className="copyright">
           <span>
             Copyright <i className="fas fa-copyright"></i> 2021 -All Rights
-            Reserved - Domain Name
+            Reserved - Kurebuddy.com
           </span>
         </div>
       </div>
